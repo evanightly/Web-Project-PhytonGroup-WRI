acb3fbbdf938257d0552fcc02a3d1020714dfff1
EvanHendersonRichtByte <adamhenderson3x3@gmail.com>
2022-01-20 23:14:04 +0700

    revisi navbar

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,23 @@
 let toggle = false;
-const log = console.log
-document.getElementById('toggler').addEventListener('click', () => {
-  toggle = !toggle
-  console.log(toggle);
+const log = console.log;
 
+const handleResponsiveNavbar = (toggle) => {
   if (!toggle) {
-    document.querySelector('body').style.overflowY = 'scroll'
-    document.getElementById("mobile").style.display = 'none'
+    document.querySelector("body").style.overflowY = "scroll";
+    document.getElementById("mobile").style.display = "none";
+    document.querySelector("#toggler > button").setAttribute("id", "");
+  } else {
+    document.querySelector("body").style.overflowY = "hidden";
+    document.getElementById("mobile").style.display = "block";
+    document.querySelector("#toggler > button").setAttribute("id", "toggled");
   }
-  else {
-    document.querySelector('body').style.overflowY = 'hidden'
-    document.getElementById("mobile").style.display = 'block'
-  }
-})
+};
 
+window.matchMedia("(min-width: 575.98px)").addEventListener("change", () => {
+  toggle = false;
+  handleResponsiveNavbar(toggle);
+});
+document.getElementById("toggler").addEventListener("click", () => {
+  toggle = !toggle;
+  handleResponsiveNavbar(toggle);
+});
